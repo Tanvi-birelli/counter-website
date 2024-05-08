905aa71e9ad6eaaffbfe7c1da2e99a78ec22b620
Tanvi-birelli <66661075+Tanvi-birelli@users.noreply.github.com>
2024-05-08 21:35:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const increaseBtn = document.getElementById("increasebtn")
+const resetBtn = document.getElementById("resetbtn")
+const decreaseBtn = document.getElementById("decreasebtn")
+const value=document.getElementById("value")
+let count=0;
+increaseBtn.addEventListener("click", ()=>{
+    count++
+    value.textContent = count
+})
+decreaseBtn.addEventListener("click", ()=>{
+    if (Number(value.textContent)>0){
+        count--
+        value.textContent = count
+    }
+})
+resetBtn.addEventListener("click", ()=>{
+    if (value.textContent !=0){
+    value.textContent=0
+    }
+})
